1cd2241df27681f960d51232c31b425c30630348
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-05-18 12:54:45 -0400

    made decorator function.

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import time
 
+current_time = time.time()
+
+
+# print(current_time)  # seconds since Jan 1st, 1970
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        beforetime = time.time()
+        function()
+        aftertime = time.time()
+        execution_time = aftertime - beforetime
+        print(execution_time)
+
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(1000000):
+        i * i
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(10000000):
+        i * i
+
+fast_function()
+slow_function()
